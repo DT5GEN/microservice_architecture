166bab82e8adca6a1142f132bf3e2f5ab378a263
Alx_Shilov <deeppowercrew@gmail.com>
2024-10-31 23:19:57 +0300

    API GateWay working

---
+++ api-gateway/src/test/java/com/deeppowercrew/microservice_architecture/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deeppowercrew.microservice_architecture.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
