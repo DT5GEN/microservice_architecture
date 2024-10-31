b41f31070594c222d774c928ab57b56286791cac
Alx_Shilov <deeppowercrew@gmail.com>
2024-11-01 02:40:43 +0300

    added ConfigServer working successfully

--- ticket-service/src/main/java/com/deeppowercrew/microservice_architecture/ticketservice/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/deeppowercrew/microservice_architecture/ticketservice/TicketServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
 public class TicketServiceApplication {
 
     public static void main(String[] args) {
